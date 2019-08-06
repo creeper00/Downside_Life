b65794afe1b8114ec0e1c18f09cd0a13ee777699
digdhg <graysea2@snu.ac.kr>
2019-08-07 01:03:39 +0900

    bug fixed

--- Downside_Life/Assets/Scripts/Canvas/RichHouse/ThiefInfoPopUp.cs
+++ Downside_Life/Assets/Scripts/Canvas/RichHouse/ThiefInfoPopUp.cs
@@ -6,11 +6,7 @@ public class ThiefInfoPopUp : MonoBehaviour
 {
     [SerializeField]
     public GameObject thiefInfo;
-    public void Awake()
-    {
-        thiefInfo = GameObject.Find("ThiefInfo");
-        thiefInfo.SetActive(false);
-    }
+   
     public void ThiefPopUp()
     {
         thiefInfo.SetActive(true);
