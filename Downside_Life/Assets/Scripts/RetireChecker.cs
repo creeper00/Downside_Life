fd07d0b5ec89e7ad541bfaf4e3323eae6e3daca9
creeper00 <rnjswldnd@snu.ac.kr>
2019-08-20 03:31:11 +0900

    Retire popup solved

---
+++ Downside_Life/Assets/Scripts/RetireChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RetireChecker : MonoBehaviour
+{
+    GameObject retireCheck;
+    GameObject retireParent;
+    // Start is called before the first frame update
+    Button retire;
+    void Start()
+    {
+        retire= this.transform.GetComponent<Button>();
+        retire.onClick.AddListener(fClick);
+    }
+
+    void fClick()
+    {
+        retireParent = GameObject.Find("Retire");
+        retireCheck = retireParent.transform.Find("RetireCheck").gameObject;
+        retireCheck.SetActive(true);
+    }
+}
---
+++ Downside_Life/Assets/Scripts/RetireOn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RetireOn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Button yes;
+    void Start()
+    {
+        yes = this.transform.GetComponent<Button>();
+        yes.onClick.AddListener(yesClick);
+    }
+
+    // Update is called once per frame
+    void yesClick()
+    {
+       GameObject.Find("RetireButton").GetComponent<RetireButton>().Retire();
+    }
+}
