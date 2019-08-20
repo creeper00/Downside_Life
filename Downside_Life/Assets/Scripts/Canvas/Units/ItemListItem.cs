69fd5b8630ddcc63678fc0f19259e817ecf37a56
digdhg <graysea2@snu.ac.kr>
2019-08-20 18:54:07 +0900

    itemListitem done

--- Downside_Life/Assets/Scripts/Canvas/Units/ItemListItem.cs
+++ Downside_Life/Assets/Scripts/Canvas/Units/ItemListItem.cs
@@ -8,11 +8,11 @@ public class ItemListItem : MonoBehaviour
     [HideInInspector]
     public int index;
     [SerializeField]
-    private Sprite crookNormal1, crookNormal2, crookRare1, crookRare2, crookRare3, crookLengendary;
+    private Sprite crookNormalBook, crookNormalMoneyPocket, crookRareBook, crookRareWax, crookLegendaryBook;
     [SerializeField]
-    private Sprite snakeNormal1, snakeNormal2, snakeRare1, snakeRare2, snakeLengendary;
+    private Sprite snakeNormalCosmetic, snakeRareCosmetic, snakeRareOnePiece, snakeLegendaryMagazineDesperate, snakeLegendaryMagazineCostIncrease, snakeLegendaryMagazineExtort;
     [SerializeField]
-    private Sprite gangNormal1, gangNormal2, gangNormal3, gangRare1, gangRare2, gangRare3, gangLengendary;
+    private Sprite gangNormalPistol, gangNormalMoneyBotarri, gangRareGun, gangRareHena, gangRareDevice, gangLegendarySekom, gangLegendaryAggro, gangLegendaryAttorny, gangLegendaryStock, gangLegendaryNormal;
 
     public void SetItemInformation(int index, Item item)
     {
@@ -26,7 +26,126 @@ public class ItemListItem : MonoBehaviour
 
         //스프라이트 붙이는 코드 작성
         //icon = XXX 하는 식으로 sprite를 붙여넣을 수 있음
-
+        if(type == 0)
+        {
+            if(grade == 0)
+            {
+                if(itemCode == 0)
+                {
+                    icon = crookNormalBook;
+                }
+                else if(itemCode == 2)
+                {
+                    icon = crookNormalMoneyPocket;
+                }
+            }
+            else if(grade == 1)
+            {
+                if (itemCode == 0)
+                {
+                    icon = crookRareBook;
+                }
+                else if (itemCode == 1)
+                {
+                    icon = crookRareWax;
+                }
+            }
+            else if(grade == 2)
+            {
+                if (itemCode == 0)
+                {
+                    icon = crookLegendaryBook;
+                }
+            }
+        }
+        else if(type == 1)
+        {
+            if (grade == 0)
+            {
+                if (itemCode == 0)
+                {
+                    icon = snakeNormalCosmetic;
+                }
+            }
+            else if (grade == 1)
+            {
+                if (itemCode == 0)
+                {
+                    icon = snakeRareCosmetic;
+                }
+                else if (itemCode == 1)
+                {
+                    icon = snakeRareOnePiece;
+                }
+            }
+            else if (grade == 2)
+            {
+                if (itemCode == 0)
+                {
+                    icon = snakeLegendaryMagazineDesperate;
+                }
+                else if (itemCode == 1)
+                {
+                    icon = snakeLegendaryMagazineCostIncrease;
+                }
+                else if (itemCode == 2)
+                {
+                    icon = snakeLegendaryMagazineExtort;
+                }
+            }
+        }
+        else if(type == 2)
+        {
+            if (grade == 0)
+            {
+                if (itemCode == 0)
+                {
+                    icon = gangNormalPistol;
+                }
+                else if (itemCode == 2)
+                {
+                    icon = gangNormalMoneyBotarri;
+                }
+            }
+            else if (grade == 1)
+            {
+                if (itemCode == 0)
+                {
+                    icon = gangRareGun;
+                }
+                else if (itemCode == 1)
+                {
+                    icon = gangRareHena;
+                }
+                else if (itemCode == 2)
+                {
+                    icon = gangRareDevice;
+                }
+            }
+            else if (grade == 2)
+            {
+                if (itemCode == 0)
+                {
+                    icon = gangLegendarySekom;
+                }
+                else if (itemCode == 1)
+                {
+                    icon = gangLegendaryAggro;
+                }
+                else if (itemCode == 2)
+                {
+                    icon = gangLegendaryAttorny;
+                }
+                else if (itemCode == 3)
+                {
+                    icon = gangLegendaryStock;
+                }
+                else if (itemCode == 4)
+                {
+                    icon = gangLegendaryNormal;
+                }
+            }
+        }
         //등급 적는 코드 작성
     }
 }
