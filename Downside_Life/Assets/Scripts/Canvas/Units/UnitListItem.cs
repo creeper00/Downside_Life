83a6efb3ee70ad0cb391d0bd6539e5e81e7623a4
vionicbest <paiden0407@gmail.com>
2019-08-21 23:02:05 +0900

    상점 판매 목록 스탯 보여줌

--- Downside_Life/Assets/Scripts/Canvas/Units/UnitListItem.cs
+++ Downside_Life/Assets/Scripts/Canvas/Units/UnitListItem.cs
@@ -14,14 +14,7 @@ public class UnitListItem : MonoBehaviour
         this.index = index;
 
         string statusText = "Lv " + crook.level;
-        switch (crook.type)
-        {
-            case 0: statusText += " 상수형"; break;
-            case 1: statusText += " 계수형"; break;
-            case 2: statusText += " 밸런스형"; break;
-            case 3: statusText += " 호구형"; break;
-        }
-        statusText += " 사기꾼";
+        statusText += crook.GetType() + " 사기꾼";
         transform.Find("Status").GetComponent<Text>().text = statusText;
         transform.Find("Attack").GetComponent<Text>().text = crook.GetRichConstantDown() + " + " + crook.GetRichRatioDown() + "%";
         transform.Find("Return").GetComponent<Text>().text = crook.GetMoneyUp() + "%";
@@ -32,22 +25,14 @@ public class UnitListItem : MonoBehaviour
         this.index = index;
 
         string statusText = "Lv " + snake.level;
-        switch (snake.type)
-        {
-            case 0: statusText += " 둔감형"; break;
-            case 1: statusText += " 갈취형"; break;
-            case 2: statusText += " 낭비형"; break;
-            case 3: statusText += " 둔화형"; break;
-            
-        }
-        statusText += " 꽃뱀";
+        statusText += snake.GetType() + " 꽃뱀";
         transform.Find("Status").GetComponent<Text>().text = statusText;
 
         string attributeText = "";
         switch (snake.type)
         {
             case 0: attributeText += "부자의 절박함 증가량 " + snake.GetDesperateControl() + "% 만큼 감소"; break;
-            case 1: attributeText += snake.GetItemPrice() + "원의 환금형 아이템 획득"; break;
+            case 1: attributeText += snake.GetLowerBound() + "만원 ~ " + snake.GetUpperBound() + "만원의 환금형 아이템 획득"; break;
             case 2: attributeText += "부자의 행동 비용 " + snake.GetBehaviorCostIncrease() + "억 원 증가"; break;
             case 3: attributeText += "부자의 행동 주기 " + snake.RichCycleIncrease() + "턴 증가"; break;
             
@@ -61,14 +46,7 @@ public class UnitListItem : MonoBehaviour
         this.index = index;
 
         string statusText = "Lv " + gang.level;
-        switch (gang.type)
-        {
-            case 0: statusText += " 딜러형"; break;
-            case 1: statusText += " 수금형"; break;
-            case 2: statusText += " 도벽형"; break;
-            case 3: statusText += " 광역형"; break;
-        }
-        statusText += " 갱단";
+        statusText += gang.GetType() + " 갱단";
         transform.Find("Status").GetComponent<Text>().text = statusText;
         transform.Find("Attack").GetComponent<Text>().text = gang.attack().ToString();
         transform.Find("Return").GetComponent<Text>().text = gang.returnMoney().ToString();
