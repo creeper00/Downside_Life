3768fb5cccd75ed4a4a67c0e85adfbb0db8f2f2b
creeper00 <rnjswldnd@snu.ac.kr>
2019-08-22 22:44:40 +0900

    Event UI made

---
+++ Downside_Life/Assets/Scripts/EventPop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPop : MonoBehaviour
+{
+    // Start is called before the first frame update\
+    [SerializeField]
+    GameObject EventPopup;
+    int set = 1;
+
+    // Update is called once per frame
+    public void showEventPopup()
+    {
+        if (set == 1)
+        {
+            EventPopup.transform.position -= new Vector3(500f, 0f, 0f);
+            set = 0;
+        }
+        else
+        {
+            EventPopup.transform.position += new Vector3(500f, 0f, 0f);
+            set = 1;
+        }
+    }
+
+}
