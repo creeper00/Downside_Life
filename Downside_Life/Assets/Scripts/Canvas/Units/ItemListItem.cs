5668aaad74075ba0a0fd385e94efa3533014b59f
digdhg <graysea2@snu.ac.kr>
2019-08-22 18:06:22 +0900

    아이템 등급 적음

--- Downside_Life/Assets/Scripts/Canvas/Units/ItemListItem.cs
+++ Downside_Life/Assets/Scripts/Canvas/Units/ItemListItem.cs
@@ -13,6 +13,21 @@ public class ItemListItem : MonoBehaviour
         this.index = index;
         transform.Find("Icon").GetComponent<Image>().sprite = item.icon;
 
+        if(item.grade == 0)
+        {
+            gameObject.transform.Find("Grade").GetComponent<Text>().text = "Normal";
+            gameObject.transform.Find("Grade").GetComponent<Text>().color = new Color32(128,128,128, 255);
+        }
+        else if(item.grade == 1)
+        {
+            gameObject.transform.Find("Grade").GetComponent<Text>().text = "Rare";
+            gameObject.transform.Find("Grade").GetComponent<Text>().color = new Color32(0, 0, 205, 255);
+        }
+        else if(item.grade == 2)
+        {
+            gameObject.transform.Find("Grade").GetComponent<Text>().text = "Legendary";
+            gameObject.transform.Find("Grade").GetComponent<Text>().color = new Color32(127, 255, 0, 255);
+        }
         //등급 적는 코드 작성
     }
 }
