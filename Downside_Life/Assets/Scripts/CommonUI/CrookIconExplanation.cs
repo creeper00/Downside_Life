d16ff4073fc0c41d97263b328966898a5a039ffb
digdhg <graysea2@snu.ac.kr>
2019-08-22 16:01:59 +0900

    사기꾼 설명 추가

---
+++ Downside_Life/Assets/Scripts/CommonUI/CrookIconExplanation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrookIconExplanation : MonoBehaviour
+{
+    GameObject popUp;
+    GameObject unitExplanationCanvas;
+    GameObject unitExplanationPanel;
+    GameObject unitExplanationText;
+    public void Awake()
+    {
+        popUp = GameObject.Find("Popups");
+        unitExplanationCanvas = popUp.transform.Find("UnitExplanationCanvas").gameObject;
+        unitExplanationPanel = unitExplanationCanvas.transform.Find("UnitExplanationPanel").gameObject;
+        unitExplanationText = unitExplanationPanel.transform.Find("ExplanationText").gameObject;
+    }
+    public void infopopup()
+    {
+        unitExplanationCanvas.SetActive(true);
+        if(gameObject.name == "AttackIcon" || gameObject.name == "MoneyStealIcon")
+        {
+            unitExplanationText.GetComponent<Text>().text = "부자에게서 뜯어오는 돈";
+        }
+        if(gameObject.name == "ReturnIcon" || gameObject.name == "ReturnPercentageIcon")
+        {
+            unitExplanationText.GetComponent<Text>().text = "플레이어에게 들고오는 돈";
+        }
+    }
+
+    public void infopopdown()
+    {
+        unitExplanationCanvas.SetActive(false);
+    }
+}
