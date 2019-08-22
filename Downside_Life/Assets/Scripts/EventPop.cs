87b2ed580ee753ca0547f40c60bf11baa48b29b6
creeper00 <rnjswldnd@snu.ac.kr>
2019-08-22 23:29:39 +0900

    event Ui fixed

--- Downside_Life/Assets/Scripts/EventPop.cs
+++ Downside_Life/Assets/Scripts/EventPop.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 
 public class EventPop : MonoBehaviour
 {
-    // Start is called before the first frame update\
+    // Start is called before the first frame update
     [SerializeField]
-    GameObject EventPopup;
+    GameObject eventPop;
     int set = 1;
-
     // Update is called once per frame
-    public void showEventPopup()
+    public void showEventPop()
     {
-        if (set == 1)
+        if(set==1)
         {
-            EventPopup.transform.position -= new Vector3(500f, 0f, 0f);
+            StartCoroutine("showSlowly");
             set = 0;
         }
         else
         {
-            EventPopup.transform.position += new Vector3(500f, 0f, 0f);
+            StartCoroutine("hideSlowly");
             set = 1;
         }
     }
 
+    private IEnumerator showSlowly()
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            eventPop.transform.position -= new Vector3(50f, 0f, 0f);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    private IEnumerator hideSlowly()
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            eventPop.transform.position += new Vector3(50f, 0f, 0f);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
 }
