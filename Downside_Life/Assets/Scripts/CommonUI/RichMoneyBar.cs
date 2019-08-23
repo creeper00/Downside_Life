ee63832a91fd0765176502d9c13abba9cc91ba6a
AlongwithKiman <expfunsce@snu.ac.kr>
2019-08-23 15:56:10 +0900

    내인생이 레전드

--- Downside_Life/Assets/Scripts/CommonUI/RichMoneyBar.cs
+++ Downside_Life/Assets/Scripts/CommonUI/RichMoneyBar.cs
@@ -9,7 +9,7 @@ public class RichMoneyBar : MonoBehaviour
     private int barWidth;
     public void ChangeBar(int current, int max)
     {
-        transform.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(barWidth * current / max, 720f);
+        transform.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(1000f * current / max, 720f);
         transform.GetChild(2).GetComponent<Text>().text = "부자의 재산: " + current + "만 원";
     }
 }
